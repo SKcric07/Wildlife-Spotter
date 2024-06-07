0e6d2421701aa0f0446c1dc506e7f559b23229e2
pavanreddyemani <p.emani@student.fdu.edu>
2024-06-07 13:50:44 -0700

    Made changes to add an API and verify its working & general database configuration for a postgressql is done

--- wildLifeSpotterBE/wildLifeSpotterBE/settings.py
+++ wildLifeSpotterBE/wildLifeSpotterBE/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "api",
+    "database",
+    "rest_framework",
+
 ]
 
 MIDDLEWARE = [
@@ -75,8 +79,11 @@ WSGI_APPLICATION = "wildLifeSpotterBE.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME" : "Dev",
+        "USER" : 'postgres',
+        "PASSWORD" : "postgrespassword",
+        "HOST" : 'localhost'
     }
 }
 
